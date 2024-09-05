d1d1b7f171d9a21505269484291d2c7a4b88fe76
botketan <ketansingh918@gmail.com>
2024-09-05 22:07:58 +0530

    Pic Change

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import Laptop from '../assets/laptop.jpg';
+import BeautyPic from '../assets/BeautyPic.jpg';
 
 const Analytics = () => {
   return (
     <div className='w-full bg-white py-16 px-4'>
       <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
-        <img className='w-[500px] mx-auto my-4' src={Laptop} alt='/' />
+        <img className='w-[500px] mx-auto my-4' src={BeautyPic} alt='/' />
         <div className='flex flex-col justify-center'>
-          <p className='text-[#00df9a] font-bold '>DATA ANALYTICS DASHBOARD</p>
-          <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Manage Data Analytics Centrally</h1>
+          <p className='text-[#00df9a] font-bold '>Beautify</p>
+          <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Beauty inspired by you.</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-            molestiae delectus culpa hic assumenda, voluptate reprehenderit
-            dolore autem cum ullam sed odit perspiciatis. Doloribus quos velit,
-            eveniet ex deserunt fuga?
+          Welcome to Beautijess, where elegance meets innovation. Our luxury cosmetics brand is dedicated to empowering women to unleash their inner radiance. With high-performance formulas, exquisite packaging, and expertly crafted shades, our products combine artistry and science to reveal your timeless beauty. Indulge in our curated collection of lipsticks, foundations, and skincare, and discover a world of sophistication and glamour.
           </p>
           <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3'>Get Started</button>
         </div>
